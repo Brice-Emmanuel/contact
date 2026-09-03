82e9b0fe2586a588512fdeaaf224b7cd6d295f42
EMMANUEL <emmanuelnyamsi721@gmail.com>
2026-09-03 03:47:49 +0200

    Fix: renommer la route login de l'API en api.login pour eviter les conflits

--- routes/api.php
+++ routes/api.php
@@ -13,31 +13,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 // --- 1. ROUTES PUBLIQUES ---
-Route::post('/register', [authController::class, 'register']);
-Route::post('/login', [authController::class, 'login'])->name('login');
+Route::post('/register', [authController::class, 'register'])->name('api.register');
+Route::post('/login', [authController::class, 'login'])->name('api.login');
 
 
 // --- 2. ROUTES PROTÉGÉES (Nécessitent le Token Bearer) ---
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::post('/logout', [authController::class, 'logout']);
+    Route::post('/logout', [authController::class, 'logout'])->name('api.logout');
 
     // --- GESTION DES CONTACTS ---
-    Route::prefix('contacts')->group(function () {
-        // La création de contact DOIT être ici pour que auth()->id() fonctionne !
-        Route::post('/', [contactController::class, 'store']);
-        Route::get('/', [contactController::class, 'index']);
+    Route::prefix('contacts')->name('api.contacts.')->group(function () {
+        Route::post('/', [contactController::class, 'store'])->name('store');
+        Route::get('/', [contactController::class, 'index'])->name('index');
         
-        // Vos autres routes...
-        Route::get('/anniversaire', [contactController::class, 'anniversaire']);
-        Route::get('/anniversairedans7jours', [contactController::class, 'anniversaire_prevu']);
-        Route::get('/statistiques/count', [contactController::class, 'statistiques']);
-        Route::put('/favoris/{id}', [contactController::class, 'favoris']);
-        Route::put('/restauration/{id}', [contactController::class, 'restauration']);
-        Route::delete('/supprimerdeff/{id}', [contactController::class, 'forceDestroy']);
-        Route::get('/{id}', [contactController::class, 'show']);
-        Route::put('/{id}', [contactController::class, 'update']);
-        Route::delete('/{id}', [contactController::class, 'destroy']);
+        Route::get('/anniversaire', [contactController::class, 'anniversaire'])->name('anniversaire');
+        Route::get('/anniversairedans7jours', [contactController::class, 'anniversaire_prevu'])->name('anniversaire.prevu');
+        Route::get('/statistiques/count', [contactController::class, 'statistiques'])->name('stats');
+        Route::put('/favoris/{id}', [contactController::class, 'favoris'])->name('favoris');
+        Route::put('/restauration/{id}', [contactController::class, 'restauration'])->name('restauration');
+        Route::delete('/supprimerdeff/{id}', [contactController::class, 'forceDestroy'])->name('force-delete');
+        Route::get('/{id}', [contactController::class, 'show'])->name('show');
+        Route::put('/{id}', [contactController::class, 'update'])->name('update');
+        Route::delete('/{id}', [contactController::class, 'destroy'])->name('destroy');
     });
 
 });
